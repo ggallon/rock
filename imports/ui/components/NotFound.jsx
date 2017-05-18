29aa8a2a04b0f85ce073a25f3a06ec1d98878412
Gwenaël Gallon <gwenael.gallon@mac.com>
2017-05-19 00:30:15 +0200

    Translated to French UI component NoFound

--- imports/ui/components/NotFound.jsx
+++ imports/ui/components/NotFound.jsx
@@ -4,7 +4,7 @@ import { Alert } from 'react-bootstrap';
 const NotFound = () => (
   <div className="NotFound">
     <Alert bsStyle="danger">
-      <p><strong>Error [404]</strong>: { window.location.pathname } does not exist.</p>
+      <p><strong>[404] Erreur </strong>: { window.location.pathname } n'existe pas.</p>
     </Alert>
   </div>
 );
