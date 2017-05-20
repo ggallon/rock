2f13ca7a1b4614b6383897a007395c28a9bf130b
Gwenaël Gallon <gwenael.gallon@mac.com>
2017-05-21 01:33:44 +0200

    Update browser policy rules

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,3 +1,6 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
-
+BrowserPolicy.content.allowConnectOrigin("wss:");
+BrowserPolicy.content.allowConnectOrigin("ws:");
 BrowserPolicy.content.allowFontOrigin('data:');
+BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
+
