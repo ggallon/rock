a05911938d0ca090a4677292375a8301265f273e
Gwenaël Gallon <gwenael.gallon@mac.com>
2017-05-22 00:14:16 +0200

    Clean code of browser policy file

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,6 +1,7 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
-BrowserPolicy.content.allowConnectOrigin("wss:");
-BrowserPolicy.content.allowConnectOrigin("ws:");
+
+BrowserPolicy.content.allowConnectOrigin('wss:');
+BrowserPolicy.content.allowConnectOrigin('ws:');
 BrowserPolicy.content.allowFontOrigin('data:');
-BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
+BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
 
