581e416916bc9b5b3c0ea8c4d1b043c2ef0e6800
Gwenaël Galon <gwenael.gallon@mac.com>
2017-06-08 00:31:01 +0200

    Clean login component

--- imports/modules/app/components/Login.jsx
+++ imports/modules/app/components/Login.jsx
@@ -1,19 +1,18 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import AutoForm from 'uniforms-bootstrap3/AutoForm';
 import { SubmitField } from 'uniforms-bootstrap3';
-import LoginSchema  from '/imports/modules/app/lib/loginSchema';
+import LoginSchema from '/imports/modules/app/lib/loginSchema';
 
 class Login extends Component {
   constructor() {
-    super(...arguments);
-    this.state = {
-      loginError: null
-    };
-    this.onChange        = this.onChange.bind(this);
-    this.onSubmit        = this.onSubmit.bind(this);
+    super();
+    this.state = { loginError: null };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
     this.onSubmitFailure = this.onSubmitFailure.bind(this);
     this.onSubmitSuccess = this.onSubmitSuccess.bind(this);
   }
@@ -25,10 +24,10 @@ class Login extends Component {
   onSubmit({ email, password }) {
     return new Promise((resolve, reject) =>
       Meteor.loginWithPassword(email, password, error =>
-        error ? reject(error) : resolve()
-      )
+        error ? reject(error) : resolve(),
+      ),
     );
-  };
+  }
 
   onSubmitFailure(error) {
     this.setState({ loginError: error });
@@ -51,16 +50,18 @@ class Login extends Component {
         <Row>
           <Col xs={12} sm={5} md={4} lg={4} className="center-block">
             <h4 className="page-header">Connexion à Rock</h4>
-            <AutoForm schema={ LoginSchema }
-                      placeholder
-                      label={ false }
-                      error={ this.state.loginError }
-                      onChange={ this.onChange }
-                      onSubmit={ this.onSubmit }
-                      onSubmitFailure={ this.onSubmitFailure }
-                      onSubmitSuccess={ this.onSubmitSuccess }
-                      submitField={ CustomSubmitField }>
-             </AutoForm>
+            <AutoForm
+              schema={LoginSchema}
+              placeholder
+              label={false}
+              error={this.state.loginError}
+              onChange={this.onChange}
+              onSubmit={this.onSubmit}
+              onSubmitFailure={this.onSubmitFailure}
+              onSubmitSuccess={this.onSubmitSuccess}
+              submitField={CustomSubmitField}
+            >
+            </AutoForm>
             <p><Link to="/recover-password" className="pull-left">Mot de passe oublié ?</Link></p>
           </Col>
         </Row>
