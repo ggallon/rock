2ba999f17d2587c1fa7ff3567deff565400713b2
Gwenaël Galon <gwenael.gallon@mac.com>
2017-06-22 00:01:01 +0200

    Revert "Update UI App Layout with global props : loading, loggingIn, authenticated, name and roles"
    This reverts commit f24243e1026cce656f3b5bec2b729dfcf5fe4687.

--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
-import { Roles } from 'meteor/alanning:roles';
 import Grid from 'react-bootstrap/lib/Grid';
 
-import GlobalNavigation from '/imports/ui/components/globalNavigation';
 import PrivateRoute from '/imports/lib/private-route';
+import AppNavigation from '/imports/ui/components/appNavigation';
 
 import Index from '/imports/modules/app/components/index';
 
@@ -23,51 +19,26 @@ import Signup from '/imports/modules/app/components/signup';
 
 import NotFound from '/imports/ui/components/notFound';
 
-const App = props => (
+const App = () => (
   <BrowserRouter>
-    {!props.loading ?
-      <div className="App">
-        <GlobalNavigation {...props} />
-        <Grid>
-          <Switch>
-            <Route exact path="/" component={Index} />
-            <PrivateRoute path="/documents/new" component={NewDocument} {...props} />
-            <PrivateRoute path="/documents/:_id/edit" component={EditDocument} {...props} />
-            <PrivateRoute path="/documents/:_id" component={ViewDocument} {...props} />
-            <PrivateRoute path="/documents" component={Documents} {...props} />
-            <Route path="/login" component={Login} />
-            <Route path="/recover-password" component={RecoverPassword} />
-            <Route path="/reset-password/:token" component={ResetPassword} />
-            <Route path="/signup" component={Signup} />
-            <Route component={NotFound} />
-          </Switch>
-        </Grid>
-      </div> : ''}
+    <div>
+      <AppNavigation />
+      <Grid>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <PrivateRoute path="/documents/new" component={NewDocument} />
+          <PrivateRoute path="/documents/:_id/edit" component={EditDocument} />
+          <PrivateRoute path="/documents/:_id" component={ViewDocument} />
+          <PrivateRoute path="/documents" component={Documents} />
+          <Route path="/login" component={Login} />
+          <Route path="/recover-password" component={RecoverPassword} />
+          <Route path="/reset-password/:token" component={ResetPassword} />
+          <Route path="/signup" component={Signup} />
+          <Route component={NotFound} />
+        </Switch>
+      </Grid>
+    </div>
   </BrowserRouter>
 );
 
-App.propTypes = {
-  loading: PropTypes.bool.isRequired,
-};
-
-const getUserName = name => ({
-  string: name,
-  object: `${name.first} ${name.last}`,
-}[typeof name]);
-
-export default createContainer(() => {
-  const loggingIn = Meteor.loggingIn();
-  const user = Meteor.user();
-  const userId = Meteor.userId();
-  const loading = !Roles.subscription.ready();
-  const name = user && user.profile && user.profile.name && getUserName(user.profile.name);
-  const emailAddress = user && user.emails && user.emails[0].address;
-
-  return {
-    loading,
-    loggingIn,
-    authenticated: !loggingIn && !!userId,
-    name: name || emailAddress,
-    roles: !loading && Roles.getRolesForUser(userId),
-  };
-}, App);
+export default App;
