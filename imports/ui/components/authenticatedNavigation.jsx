3c627f8f7e3cbf3574ecb800f9af686dabbf7721
Gwenaël Galon <gwenael.gallon@mac.com>
2017-06-22 00:21:25 +0200

    Update UI AuthenticatedNavigation with name props

--- imports/ui/components/authenticatedNavigation.jsx
+++ imports/ui/components/authenticatedNavigation.jsx
@@ -1,20 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
 import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 
-const userName = () => {
-  const user = Meteor.user();
-  const name = user ? user.name : '';
-  return user ? `${name.first} ${name.last}` : '';
-};
-
-const AuthenticatedNavigation = ({ history }) => (
+const AuthenticatedNavigation = props => (
   <div>
     <Nav>
       <LinkContainer to="/documents">
@@ -22,19 +15,19 @@ const AuthenticatedNavigation = ({ history }) => (
       </LinkContainer>
     </Nav>
     <Nav pullRight>
-      <NavDropdown eventKey={3} title={userName()} id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1} onClick={() => Meteor.logout(() => history.push('/login'))}>Se déconnecter</MenuItem>
+      <NavDropdown eventKey={3} title={props.name} id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1} onClick={() => Meteor.logout()}>Se déconnecter</MenuItem>
       </NavDropdown>
     </Nav>
   </div>
 );
 
 AuthenticatedNavigation.defaultProps = {
-  history: null,
+  name: '',
 };
 
 AuthenticatedNavigation.propTypes = {
-  history: PropTypes.object,
+  name: PropTypes.string.isRequired,
 };
 
-export default withRouter(AuthenticatedNavigation);
+export default AuthenticatedNavigation;
