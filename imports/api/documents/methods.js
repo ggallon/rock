16d6eeb4d7253ed1a1e9e8f4feb0ff326b3c97b9
Gwenaël Galon <gwenael.gallon@mac.com>
2017-06-28 01:14:39 +0200

    Update API Documents, method, replaces upsertDocument with insertDocument

--- imports/api/documents/methods.js
+++ imports/api/documents/methods.js
@@ -4,13 +4,11 @@ import SimpleSchema from 'simpl-schema';
 import rateLimit from '/imports/lib/rate-limit';
 import Documents from './documents';
 
-export const upsertDocument = new ValidatedMethod({
-  name: 'documents.upsert',
+export const insertDocument = new ValidatedMethod({
+  name: 'documents.insert2',
   validate: new SimpleSchema({
-    _id: { type: String, optional: true },
-    title: { type: String, optional: true },
-    body: { type: String, optional: true },
-    owner: { type: String, optional: true },
+    title: { type: String },
+    body: { type: String },
   }).validator(),
   run(document) {
     if (!this.userId) {
@@ -18,22 +16,11 @@ export const upsertDocument = new ValidatedMethod({
         'Must be logged in to insertor update a document');
     }
 
-    if (document._id && document.owner !== this.userId) {
-      throw new Meteor.Error('documents.upsert.accessDenied',
-        'You don\'t have permission to update this document.');
-    }
-
-    const data = {
-      title: document.title,
-      body: document.body,
-    };
-
-    if (!document._id) {
-      data.createdAt = new Date();
-      data.owner = this.userId;
-    }
-
-    return Documents.upsert({ _id: document._id }, { $set: data });
+    return Documents.insert({
+      ...document,
+      owner: this.userId,
+      createdAt: new Date(),
+    });
   },
 });
 
@@ -65,7 +52,7 @@ export const removeDocument = new ValidatedMethod({
 
 rateLimit({
   methods: [
-    upsertDocument,
+    insertDocument,
     removeDocument,
   ],
   limit: 5,
