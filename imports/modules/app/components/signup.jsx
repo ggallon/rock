734b31320341162e2f0b6f8561a2835e31f943a8
Gwenaël Galon <gwenael.gallon@mac.com>
2017-06-28 02:32:42 +0200

    Update App, component Signup, fix missing import

--- imports/modules/app/components/signup.jsx
+++ imports/modules/app/components/signup.jsx
@@ -1,3 +1,4 @@
+import { Accounts } from 'meteor/accounts-base';
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
