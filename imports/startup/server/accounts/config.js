fe104f5ef767741083766be550fab2509d8228af
Gwenaël Galon <gwenael.gallon@mac.com>
2017-11-21 15:43:33 +0100

    Cleaning code

--- imports/startup/server/accounts/config.js
+++ imports/startup/server/accounts/config.js
@@ -3,10 +3,9 @@ import { Accounts } from 'meteor/accounts-base';
 Accounts.config({
   sendVerificationEmail: false,
   forbidClientAccountCreation: true,
+  passwordEnrollTokenExpirationInDays: 1,
   restrictCreationByEmailDomain: '',
   loginExpirationInDays: 7,
   passwordResetTokenExpirationInDays: 0.5,
-  passwordEnrollTokenExpirationInDays: 1,
   ambiguousErrorMessages: true,
 });
-
