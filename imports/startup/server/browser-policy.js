77443c33b379717e8a8d51aeab253e8439b30a5c
Gwenaël Galon <gwenael.gallon@mac.com>
2017-12-03 00:46:12 +0100

    Update browser policies rules

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,7 +1,19 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
 
-BrowserPolicy.content.allowConnectOrigin('wss:');
+/**
+ * Set browser policies
+ */
+if (process.env.NODE_ENV === 'development') {
+  BrowserPolicy.content.allowOriginForAll('localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('ws://localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('wss://localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('http://localhost:*');
+  BrowserPolicy.content.allowConnectOrigin('https://localhost:*');
+  BrowserPolicy.framing.allowAll();
+}
+
 BrowserPolicy.content.allowConnectOrigin('ws:');
+BrowserPolicy.content.allowConnectOrigin('wss:');
+
 BrowserPolicy.content.allowFontOrigin('data:');
 BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
-
