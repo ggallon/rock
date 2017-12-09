60d23f5e5d98afa8d16ed8ce3e2b2609bee42cb0
Gwenaël Galon <gwenael.gallon@mac.com>
2017-12-09 23:53:40 +0100

    Update browser policies rules

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,19 +1,27 @@
+import { Meteor } from 'meteor/meteor';
 import { BrowserPolicy } from 'meteor/browser-policy-common';
+
+const parsedUrl = Meteor.absoluteUrl();
+const hostname = parsedUrl.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
 
 /**
  * Set browser policies
  */
 if (process.env.NODE_ENV === 'development') {
-  BrowserPolicy.content.allowOriginForAll('localhost:*');
-  BrowserPolicy.content.allowConnectOrigin('ws://localhost:*');
-  BrowserPolicy.content.allowConnectOrigin('wss://localhost:*');
-  BrowserPolicy.content.allowConnectOrigin('http://localhost:*');
-  BrowserPolicy.content.allowConnectOrigin('https://localhost:*');
+  BrowserPolicy.content.allowOriginForAll(hostname[1] + ':*');
+  BrowserPolicy.content.allowConnectOrigin('ws://' + hostname[1] + ':*');
+  BrowserPolicy.content.allowConnectOrigin('wss://' + hostname[1] + ':*');
+  BrowserPolicy.content.allowConnectOrigin('http://' + hostname[1] + ':*');
+  BrowserPolicy.content.allowConnectOrigin('https://' + hostname[1] + ':*');
   BrowserPolicy.framing.allowAll();
 }
 
-BrowserPolicy.content.allowConnectOrigin('ws:');
-BrowserPolicy.content.allowConnectOrigin('wss:');
+BrowserPolicy.framing.disallow();
+BrowserPolicy.content.disallowInlineScripts();
 
-BrowserPolicy.content.allowFontOrigin('data:');
+BrowserPolicy.content.allowConnectOrigin('ws:' + hostname[1]);
+BrowserPolicy.content.allowConnectOrigin('wss:' + hostname[1]);
+
+BrowserPolicy.content.allowFontOrigin('data:' + hostname[1]);
+
 BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
