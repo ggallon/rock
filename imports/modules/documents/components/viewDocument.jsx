00d63bd54d44318bdcfb26aba81a4d86f91108c2
Gwenaël Galon <gwenael.gallon@mac.com>
2018-01-25 03:00:58 +0100

    Update module Documents with Dates lib

--- imports/modules/documents/components/viewDocument.jsx
+++ imports/modules/documents/components/viewDocument.jsx
@@ -7,6 +7,7 @@ import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
 import Documents from '../../../api/documents/documents';
 import container from '../../../lib/container';
+import { timeago, dayMonthYearAtTime } from '../../../lib/dates';
 import Loading from '../../../ui/components/loading';
 import NotFound from '../../../ui/components/notFound';
 
@@ -43,18 +44,18 @@ class ViewDocument extends Component {
             </Alert>
           ) : ''}
           <div className="page-header clearfix">
-            <h4 className="pull-left">{ doc && doc.title }</h4>
+            <h4 className="pull-left">{ doc.title }</h4>
             <ButtonToolbar className="pull-right">
               <ButtonGroup bsSize="small">
-                <Button onClick={() => history.push(`${match.url}/edit`)} disabled={doc && doc.owner !== user._id}>Modifier</Button>
-                <Button onClick={() => this.handleRemove(doc._id, history)} disabled={doc && doc.owner !== user._id} className="text-danger">Supprimer</Button>
+                <Button onClick={() => history.push(`${match.url}/edit`)} disabled={doc.owner !== user._id}>Modifier</Button>
+                <Button onClick={() => this.handleRemove(doc._id, history)} disabled={doc.owner !== user._id} className="text-danger">Supprimer</Button>
               </ButtonGroup>
             </ButtonToolbar>
           </div>
           <div>
-            <p>{ doc && doc.body }</p>
-            <p><i>{ doc && doc.createdAt.toString() }</i></p>
-            <p><b>{ doc && doc.updatedAt && doc.updatedAt.toString() }</b></p>
+            <p>{ doc.body }</p>
+            <p><b>{ doc.updatedAt && `Modifié ${timeago(doc.updatedAt, 'Europe/Paris')}` }</b></p>
+            <p><i>Créé le { dayMonthYearAtTime(doc.createdAt) }</i></p>
           </div>
         </div>
       ) : <NotFound />
