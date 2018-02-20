bd353b44a80a06ef24ec4f372237f1f58d9c89e3
Gwenaël Gallon <gwenael.gallon@mac.com>
2018-02-20 01:05:10 +0100

    Update API Users, move Factory in api.users.test.js, add tests for Collection and methods

--- imports/api/users/collection.js
+++ imports/api/users/collection.js
@@ -2,9 +2,3 @@ import { Meteor } from 'meteor/meteor';
 
 const Users = Meteor.users;
 export default Users;
-
-Factory.define('user', Users, {
-  givenName: () => 'Given name',
-  familyName: () => 'Family name',
-  identifiant: () => 'user@user.com',
-});
