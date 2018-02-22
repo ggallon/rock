6432be9f911da7c59fdab151c15997625eaf372e
Gwenaël Gallon <gwenael.gallon@mac.com>
2018-02-22 14:47:38 +0100

    Update UI, add new module Calendar in Layout private route

--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -5,25 +5,28 @@ import Grid from 'react-bootstrap/lib/Grid';
 import LoadableWrapper from '../components/loadableWrapper';
 
 import GlobalNavigation from '../components/globalNavigation';
-import PublicRoute from '../components/publicRoute';
-import PrivateRoute from '../components/privateRoute';
-import NotFound from '../components/notFound';
+import PublicRoute      from '../components/publicRoute';
+import PrivateRoute     from '../components/privateRoute';
+import NotFound         from '../components/notFound';
 
-const Index = LoadableWrapper({ loader: () => import('../../modules/app/components/index') });
-const Login = LoadableWrapper({ loader: () => import('../../modules/app/components/login') });
+// App module
+const Index           = LoadableWrapper({ loader: () => import('../../modules/app/components/index') });
+const Login           = LoadableWrapper({ loader: () => import('../../modules/app/components/login') });
 const RecoverPassword = LoadableWrapper({ loader: () => import('../../modules/app/components/recoverPassword') });
-const ResetPassword = LoadableWrapper({ loader: () => import('../../modules/app/components/resetPassword') });
-const Signup = LoadableWrapper({ loader: () => import('../../modules/app/components/signup') });
-
-const Quotes = LoadableWrapper({ loader: () => import('../../modules/quotes/components/quotes') });
-const NewQuote = LoadableWrapper({ loader: () => import('../../modules/quotes/components/newQuote') });
-const ViewQuote = LoadableWrapper({ loader: () => import('../../modules/quotes/components/viewQuote') });
-const EditQuote = LoadableWrapper({ loader: () => import('../../modules/quotes/components/editQuote') });
-
-const Users = LoadableWrapper({ loader: () => import('../../modules/users/components/users') });
-const NewUser = LoadableWrapper({ loader: () => import('../../modules/users/components/newUser') });
-const ViewUser = LoadableWrapper({ loader: () => import('../../modules/users/components/viewUser') });
-const EditUser = LoadableWrapper({ loader: () => import('../../modules/users/components/editUser') });
+const ResetPassword   = LoadableWrapper({ loader: () => import('../../modules/app/components/resetPassword') });
+const Signup          = LoadableWrapper({ loader: () => import('../../modules/app/components/signup') });
+// Quotes module
+const Quotes          = LoadableWrapper({ loader: () => import('../../modules/quotes/components/quotes') });
+const NewQuote        = LoadableWrapper({ loader: () => import('../../modules/quotes/components/newQuote') });
+const ViewQuote       = LoadableWrapper({ loader: () => import('../../modules/quotes/components/viewQuote') });
+const EditQuote       = LoadableWrapper({ loader: () => import('../../modules/quotes/components/editQuote') });
+// Calendar module
+const Calendar        = LoadableWrapper({ loader: () => import('../../modules/calendar/components/calendar') });
+// Users module
+const Users           = LoadableWrapper({ loader: () => import('../../modules/users/components/users') });
+const NewUser         = LoadableWrapper({ loader: () => import('../../modules/users/components/newUser') });
+const ViewUser        = LoadableWrapper({ loader: () => import('../../modules/users/components/viewUser') });
+const EditUser        = LoadableWrapper({ loader: () => import('../../modules/users/components/editUser') });
 
 const App = props => (
   <BrowserRouter>
@@ -37,6 +40,7 @@ const App = props => (
             <PrivateRoute path="/quotes/:_id/edit" component={EditQuote} {...props} />
             <PrivateRoute path="/quotes/:_id" component={ViewQuote} {...props} />
             <PrivateRoute path="/quotes" component={Quotes} {...props} />
+            <PrivateRoute path="/calendar" component={Calendar} {...props} />
             <PrivateRoute path="/users/new" component={NewUser} {...props} />
             <PrivateRoute path="/users/:_id/edit" component={EditUser} {...props} />
             <PrivateRoute path="/users/:_id" component={ViewUser} {...props} />
